8f36f555e3bbef050eab28d0ea6e7262fa501610
brugobi <brgobbi@hotmail.com>
2021-04-20 09:00:14 -0400

    add another hook example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HookCounter />
+      {/* <HookCounter /> */}
+      <HookCounterTwo />
     </div>
   );
 }
