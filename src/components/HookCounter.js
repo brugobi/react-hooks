273ac2e8e956bb30ae75ac1f615bcb1803a6c1b9
brugobi <brgobbi@hotmail.com>
2021-04-20 17:06:54 -0400

    add conditionally run effects to the HookCounter

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -3,14 +3,23 @@ import React, { useState, useEffect } from 'react';
 function HookCounter() {
 
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   // run every render
+  // useEffect(() => {
+  //   console.log('useEffect - Updating document title')
+  //   document.title = `You clicked ${count} times`
+  // })
+
+  // run just when count is updated, this avoid unnecessary render
   useEffect(() => {
+    console.log('useEffect - Updating document title')
     document.title = `You clicked ${count} times`
-  })
+  }, [count])
 
   return (
     <div>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>Counter {count}</button>
     </div>
   )
