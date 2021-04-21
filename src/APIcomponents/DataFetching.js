a24be6a6f98409e44d0b289053b5045d86cd6bc4
brugobi <brgobbi@hotmail.com>
2021-04-21 11:29:08 -0400

    update the DataFetching to fetch a individualy post using a button and onClick event

--- src/APIcomponents/DataFetching.js
+++ src/APIcomponents/DataFetching.js
@@ -4,6 +4,11 @@ import React, { useState, useEffect } from 'react'
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  };
 
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -16,12 +21,13 @@ function DataFetching() {
         console.log(err)
       })
     // the data will be fatching in a infinite loop if you dont add an empty []
-  }, [id])
+  }, [idFromButtonClick])
 
 
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button onClick={handleClick}>Fetch Post</button>
       <div>{post.title}</div>
       {/* <ul>
         {
