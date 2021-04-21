2eca7e51ee133548b9c65334ba06af4a094c25b9
brugobi <brgobbi@hotmail.com>
2021-04-21 11:17:49 -0400

    update the DataFetching to fetch a individualy post by passing the id passed by the user

--- src/APIcomponents/DataFetching.js
+++ src/APIcomponents/DataFetching.js
@@ -2,29 +2,32 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then(res => {
         console.log(res)
         // this update the posts variable that will be render in the UI
-        setPosts(res.data)
+        setPost(res.data)
       })
       .catch(err => {
         console.log(err)
       })
     // the data will be fatching in a infinite loop if you dont add an empty []
-  }, [])
+  }, [id])
 
 
   return (
     <div>
-      <ul>
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {/* <ul>
         {
           posts.map(post => <li key={post.id}>{post.title}</li>)
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
